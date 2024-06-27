50f1563305559fc38538d3dac0d1eda441b7adc2
Guilherme Camillo <guicamillo@gmail.com>
2024-06-27 08:46:36 -0700

    cleaning up a ton of dead code

---
+++ src/utils/get-all-files-from-folder.ts
@@ -0,0 +1,67 @@
+import type { LinkDetails } from "@components/link-list.astro";
+
+type Glob = {
+  default: string;
+};
+
+const YMD_REGEXP = /(\d{4})\-(\d{2})\-(\d{2})/;
+
+const isSMM = (s: string) => s.startsWith("/SMM/");
+const isAGM = (s: string) => s.startsWith("/AGM/");
+const isFinancialStatement = (s: string) => s.startsWith("/FINANCIAL_STATEMENTS/");
+
+const removePublicFolder = (s: string) => s.replace("/public", "");
+
+const MONTHS = (() => {
+  const format = new Intl.DateTimeFormat("en-US", { month: "long" }).format;
+  return [0, ...Array(12).keys()].map((m) => format(new Date(Date.UTC(2021, (m + 1) % 12))));
+})();
+
+export function toHumanReadableFormat(input: string) {
+  const cleanedInput = removePublicFolder(input);
+  const year = cleanedInput.match(YMD_REGEXP)?.[1];
+  const month = cleanedInput.match(YMD_REGEXP)?.[2];
+
+  if (isSMM(cleanedInput)) {
+    const special = cleanedInput.toLocaleLowerCase().includes("special") ? " <b>Special</b> " : "";
+    return `${MONTHS[parseInt(month!)]} &mdash; ${special}Strata Meeting Minutes`;
+  }
+
+  if (isAGM(cleanedInput)) {
+    return `${year} AGM Meeting Minutes`;
+  }
+
+  if (isFinancialStatement(cleanedInput)) {
+    return `${year} Financial Statement &mdash; Year End`;
+  }
+
+  return cleanedInput;
+}
+
+export function groupStrataMeetingMinutesPerYear(entries: Glob[]) {
+  const groupedContents: Record<string, string[]> = {};
+
+  entries.forEach((e) => {
+    const cleanedFileName = removePublicFolder(e.default);
+    const parsedYear = cleanedFileName.match(YMD_REGEXP)?.[1];
+    if (!parsedYear) throw new Error("Could not extract date from: " + cleanedFileName);
+
+    if (!Array.isArray(groupedContents[parsedYear])) {
+      groupedContents[parsedYear] = [];
+    }
+
+    groupedContents[parsedYear].push(cleanedFileName);
+    groupedContents[parsedYear].sort();
+  });
+
+  return groupedContents;
+}
+
+export function convertGlobToLinkList(entries: Glob[]): LinkDetails[] {
+  return entries.reverse().map((e) => {
+    return {
+      href: removePublicFolder(e.default),
+      text: toHumanReadableFormat(e.default),
+    };
+  });
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,16 +2,11 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-
+  darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
-        sans: [
-          "Bricolage Grotesque Variable",
-          "Inter Variable",
-          "Inter",
-          ...defaultTheme.fontFamily.sans,
-        ],
+        sans: ["Bricolage Grotesque Variable", "Inter Variable", "Inter", ...defaultTheme.fontFamily.sans],
       },
     },
   },
