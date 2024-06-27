c9cbca084f7c69ec1111c98260e5ac92595a6603
Guilherme Camillo <guicamillo@gmail.com>
2024-06-27 08:46:36 -0700

    dx: fixes prettier config

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -3,4 +3,12 @@
 export default {
   plugins: ["prettier-plugin-astro"],
   printWidth: 120,
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
 };
